78607dff8c8dd906c8cc74ae1a3c4ec215dbfe03
amotorin <motorinas@gmail.com>
2022-06-06 17:00:27 +0300

    addChange

--- src/org/motorin/git/Main.java
+++ src/org/motorin/git/Main.java
@@ -2,6 +2,7 @@ package org.motorin.git;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Branch2");
         System.out.println("Hello git");
     }
 }
